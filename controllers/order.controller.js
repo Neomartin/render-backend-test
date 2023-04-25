a9de6c76fbc4e376fb76a2eaf1cf6498ee8d8079
Martin Rodriguez <neomartinr@gmail.com>
2023-04-25 18:58:15 -0300

    Orders crud finished

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,56 +1,97 @@
 const Order = require('../schemas/order.schema');
+const Product = require('../schemas/product.schema')
 
-async function saveOrder(req, res) {
+
+const createOrder = async (req, res) => {
     try {
-        console.log(req.body)
         const order = new Order(req.body);
-        console.log(order)
-        const newOrder =  await order.save();
-        if(!newOrder) return res.status(401).send({
-            ok: false,
-            msg: `Error al crear la nueva orden`
-        })
-        return res.status(200).send({
-            ok: true,
-            order,
-            msg: `Orden creada correctamente`
-        })
+        order.totalPrice = await verifyOrderAndCalculate(req.body.orderProducts);
+
+        //TODO: remove
+        order.userId = req.user?._id || '6413b9ebe6388a06e13905d9';
+        
+        const newOrder = await order.save();
+
+        if(!newOrder) throw new Error('Error al crear la orden');
+
+        return res.status(200).send(newOrder)
+        
     } catch (error) {
         console.log(error);
-        return res.status(401).send({
-            ok: false,
-            msg: `Error al crear la nueva orden`,
-        })
-    }
-
-} 
-
-async function getOrder(req, res) {
-    const query = req.params.id ? Order.findById(req.params.id ) : Order.find();
-    try {
-        const id = req.params.id;
-        const order = await query.populate('products.productId').populate('userId', { password: 0 });
-        if(!order) return res.status(401).send({
-            ok: false,
-            msg: `No se encontró la orden con el id ${id}`
-        });
-
-        return res.status(200).send({
-            ok: true,
-            order,
-            msg: `Orden encontrada correctamente`
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(401).send({
-            ok: false,
-            msg: `Error al obtener la orden`,
-        })
+        return res.status(500).send(error instanceof Error ? error.message : 'Error al crear la orden')
     }
 }
 
+async function getOrders(req, res) {
+    try {
+        const orders = await Order.find().populate('orderProducts.product', { name: 1, price: 1 });
+        if(!orders) throw new Error('Error al obtener las ordenes');
+
+        return res.status(200).send(orders)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error instanceof Error ? error.message : 'Error al obtener las ordenes')
+    }
+}
+
+async function getOrderById(req, res) {
+    try {
+        const id = req.params.id;
+        const order = await Order.findById(id).populate('orderProducts.product', { name: 1, price: 1 });
+        if(!order) throw new Error('Error al obtener la orden');
+        return res.status(200).send(order)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error instanceof Error ? error.message : 'Error al obtener la orden')
+    }
+
+}
+
+async function verifyOrderAndCalculate(orderProducts) {
+        let total = 0;	
+        for (const prod of orderProducts) {
+            const product = await Product.findById(prod.product);
+
+            if(!product) throw new Error('Producto no encontrado');
+    
+            // check stock
+            // if(product.stock < orderProduct.quantity) throw new Error('No hay suficiente stock');
+            if(product.price !== prod.price) throw new Error('El precio del producto no coincide');
+            total += prod.price * prod.quantity;
+        };
+        return total;
+}
+
+async function deleteOrder(req, res) {
+    try {
+        const id = req.params.id;
+        const deletedOrder = await Order.findByIdAndDelete(id);
+        if(!deletedOrder) throw new Error('Error al eliminar la orden');
+        return res.status(200).send(deletedOrder)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error instanceof Error ? error.message : 'Error al eliminar la orden')
+    }
+}
+
+async function updateOrder(req, res) {
+    try {
+        const id = req.params.id;
+        const order = req.body;
+        order.totalPrice = await verifyOrderAndCalculate(order.orderProducts);
+        const updatedOrder = await Order.findByIdAndUpdate(id, order, { new: true });
+        if(!updatedOrder) throw new Error('Error al actualizar la orden');
+        return res.status(200).send(updatedOrder)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error instanceof Error ? error.message : 'Error al actualizar la orden')
+    }
+}
 
 module.exports = {
-    saveOrder,
-    getOrder
+    createOrder,
+    getOrderById,
+    getOrders,
+    deleteOrder,
+    updateOrder
 }
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,8 +2,20 @@
 const express = require('express');
 const api = express.Router();
 const orderController = require('../controllers/order.controller');
+const express = require('express');
+const router = express.Router();
 
-api.get('/order/:id?', orderController.getOrder);
-api.post('/order', orderController.saveOrder);
+const orderController = require('../controllers/order.controller');
+//Get order by id
+router.get('/orders/:id', orderController.getOrderById);
+//Get orders
+router.get('/orders', orderController.getOrders);
+//Post order
+router.post('/orders', orderController.createOrder);
+//Put order
+router.put('/orders/:id', orderController.updateOrder);
+//Delete order
+router.delete('/orders/:id', orderController.deleteOrder)
+
 
 module.exports = api;
